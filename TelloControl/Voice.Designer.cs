36f63f63540f8122d1ab1f42a1e6263b03d686c2
Asiri Dissanayaka <asiridissa@live.com>
2024-01-28 12:11:11 +0530

    Google Speech to text GUI

---
+++ TelloControl/Voice.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TelloControl
+{
+    partial class Voice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            console = new RichTextBox();
+            siLK = new Button();
+            enUS = new Button();
+            SuspendLayout();
+            // 
+            // console
+            // 
+            console.Location = new Point(134, 2);
+            console.Name = "console";
+            console.Size = new Size(666, 447);
+            console.TabIndex = 0;
+            console.Text = "";
+            // 
+            // siLK
+            // 
+            siLK.Location = new Point(26, 12);
+            siLK.Name = "siLK";
+            siLK.Size = new Size(75, 23);
+            siLK.TabIndex = 1;
+            siLK.Text = "si-LK";
+            siLK.UseVisualStyleBackColor = true;
+            siLK.Click += siLK_Click;
+            // 
+            // enUS
+            // 
+            enUS.Location = new Point(26, 51);
+            enUS.Name = "enUS";
+            enUS.Size = new Size(75, 23);
+            enUS.TabIndex = 2;
+            enUS.Text = "en-US";
+            enUS.UseVisualStyleBackColor = true;
+            enUS.Click += enUS_Click;
+            // 
+            // Voice
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(enUS);
+            Controls.Add(siLK);
+            Controls.Add(console);
+            Name = "Voice";
+            Text = "Voice";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox console;
+        private Button siLK;
+        private Button enUS;
+    }
+}
---
+++ TelloControl/Voice.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualBasic;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Speech.V1;
+using NAudio.Wave;
+
+namespace TelloControl
+{
+
+    public partial class Voice : Form
+    {
+        private WaveInEvent waveIn;
+        private SpeechClient speechClient;
+        private SpeechClient.StreamingRecognizeStream streamingCall;
+
+        public Voice()
+        {
+            InitializeComponent();
+            // Path to your Google Cloud service account key file
+            string credentialPath = "msc2024-411517-71868ce380c1.json";
+
+            // Set the environment variable to authenticate
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+            speechClient = SpeechClient.Create();
+        }
+
+        private async void siLK_Click(object sender, EventArgs e)
+        {
+            await StartRecognition("si-LK");
+        }
+
+        private async void enUS_Click(object sender, EventArgs e)
+        {
+            await StartRecognition("en-US");
+        }
+
+        public async Task StartRecognition(string languageCode)
+        {
+            console.AppendText($"{languageCode} Starting...\n");
+            if (waveIn != null)
+            {
+                // Stop current recording if any
+                waveIn.StopRecording();
+                waveIn.Dispose();
+            }
+
+            var config = new RecognitionConfig
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRateHertz = 16000,
+                LanguageCode = languageCode
+            };
+
+            var streamingConfig = new StreamingRecognitionConfig
+            {
+                Config = config,
+                InterimResults = true
+            };
+
+            streamingCall = speechClient.StreamingRecognize();
+            await streamingCall.WriteAsync(new StreamingRecognizeRequest
+            {
+                StreamingConfig = streamingConfig
+            });
+
+            waveIn = new WaveInEvent
+            {
+                WaveFormat = new WaveFormat(16000, 1)
+            };
+            waveIn.DataAvailable += async (sender, args) =>
+            {
+                await streamingCall.WriteAsync(new StreamingRecognizeRequest
+                {
+                    AudioContent = Google.Protobuf.ByteString.CopyFrom(args.Buffer, 0, args.BytesRecorded)
+                });
+            };
+
+            waveIn.StartRecording();
+            console.AppendText("Speak now...\n");
+
+            Task.Run(async () =>
+            {
+                await foreach (var response in streamingCall.GetResponseStream())
+                {
+                    foreach (var result in response.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                console.AppendText($"Transcript: {alternative.Transcript}, Confidence: {alternative.Confidence}\n");
+                            }));
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
